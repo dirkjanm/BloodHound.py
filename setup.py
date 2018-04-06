ccc42b3fedd846a8cdfbccc1ed8416a1120b9240
Dirkjan Mollema <dirkjan.mollema@fox-it.com>
2018-04-06 13:04:18 +0200

    Update dependencies - v0.2.0

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name='bloodhound',
-      version='0.1.0',
+      version='0.2.0',
       description='Python based ingestor for BloodHound',
       author='Edwin van Vliet, Dirk-jan Mollema, Matthijs Gielen',
       author_email='edwin.vanvliet@fox-it.com, dirkjan.mollema@fox-it.com, matthijs.gielen@fox-it.com',
       url='https://github.com/fox-it/bloodhound.py',
       packages=['bloodhound'],
       license='MIT',
-      install_requires=['dnspython','impacket'],
+      install_requires=['dnspython', 'impacket', 'ldap3'],
       classifiers=[
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: MIT License',
