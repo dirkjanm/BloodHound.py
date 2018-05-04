9d243fc5844e6baa0a51119964d4548a6fee994b
Dirkjan Mollema <dirkjan.mollema@fox-it.com>
2018-05-04 12:42:53 +0200

    v0.3.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bloodhound',
-      version='0.3.1',
+      version='0.3.2',
       description='Python based ingestor for BloodHound',
       author='Edwin van Vliet, Dirk-jan Mollema, Matthijs Gielen',
       author_email='edwin.vanvliet@fox-it.com, dirkjan.mollema@fox-it.com, matthijs.gielen@fox-it.com',
