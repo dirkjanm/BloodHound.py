208104896b077ed44427c08a8d5b10b1af14b78d
Dirkjan Mollema <dirkjan.mollema@fox-it.com>
2019-01-08 15:17:00 +0100

    Force ldap3 version to 2.5.1 because 2.5.2 is broken

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bloodhound',
-      version='0.6.0',
+      version='0.6.1',
       description='Python based ingestor for BloodHound',
       author='Dirk-jan Mollema, Edwin van Vliet, Matthijs Gielen',
       author_email='dirkjan.mollema@fox-it.com, edwin.vanvliet@fox-it.com, matthijs.gielen@fox-it.com',
@@ -12,7 +12,7 @@ setup(name='bloodhound',
                 'bloodhound.ad',
                 'bloodhound.enumeration'],
       license='MIT',
-      install_requires=['dnspython', 'impacket>=0.9.17', 'ldap3>=2.5.0', 'pyasn1>=0.4'],
+      install_requires=['dnspython', 'impacket>=0.9.17', 'ldap3==2.5.1', 'pyasn1>=0.4'],
       classifiers=[
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: MIT License',
