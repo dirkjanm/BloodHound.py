fd793b94d6e3a4238c5451ab76915bce59ae5ed0
Rob Fuller <mubix@users.noreply.github.com>
2020-06-10 03:56:07 -0400

    Increase Timeout and Auto_Range (#51)
    These two tweaks will help with proxied or slow connections.
    1. 60 seconds is just arbitrarily larger and helped in my specific case where proxychains over a slow HTTPS connection was making the results timeout.
    2. auto_range allows LDAP3 to pull in more results to Bloodhound.py at one time. This increases the RAM hit, but also decreases timeouts and errors.

--- bloodhound/ad/authentication.py
+++ bloodhound/ad/authentication.py
@@ -60,7 +60,7 @@ class ADAuthentication(object):
         else:
             ldappass = self.password
         ldaplogin = '%s\\%s' % (self.domain, self.username)
-        conn = Connection(server, user=ldaplogin, auto_referrals=False, password=ldappass, authentication=NTLM, receive_timeout=30)
+        conn = Connection(server, user=ldaplogin, auto_referrals=False, password=ldappass, authentication=NTLM, receive_timeout=60, auto_range=True)
 
         # TODO: Kerberos auth for ldap
         if self.kdc is not None:
