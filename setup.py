1214af8de553b157193d32d429c0fdc6dcc7e415
Dirk-jan Mollema <dirkjan@outsidersecurity.nl>
2022-11-28 18:10:05 +0100

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bloodhound',
-      version='1.5.0',
+      version='1.6.0',
       description='Python based ingestor for BloodHound',
       author='Dirk-jan Mollema, Edwin van Vliet, Matthijs Gielen',
       author_email='dirkjan@dirkjanm.io, edwin.vanvliet@fox-it.com, matthijs.gielen@fox-it.com',
