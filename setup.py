026d03e4905bb7e43b3bad96a6c9722350506929
Dirk-jan Mollema <dirkjan@outsidersecurity.nl>
2023-12-01 16:04:11 +0100

    version update

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='bloodhound',
-      version='1.6.1',
+      version='1.7.0',
       description='Python based ingestor for BloodHound',
       author='Dirk-jan Mollema, Edwin van Vliet, Matthijs Gielen',
       author_email='dirkjan@dirkjanm.io, edwin.vanvliet@fox-it.com, matthijs.gielen@fox-it.com',
       maintainer='Dirk-jan Mollema',
       maintainer_email='dirkjan@dirkjanm.io',
-      url='https://github.com/fox-it/bloodhound.py',
+      url='https://github.com/dirkjanm/bloodhound.py',
       packages=['bloodhound',
                 'bloodhound.ad',
                 'bloodhound.lib',
@@ -17,10 +17,12 @@ setup(name='bloodhound',
       classifiers=[
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
       ],
       entry_points= {
         'console_scripts': ['bloodhound-python=bloodhound:main']
